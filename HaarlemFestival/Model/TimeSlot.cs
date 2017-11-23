5f245baced5ed184741c5c83afece44338585f66
Joost96 <joost-k@live.nl>
2017-11-23 13:17:06 +0100

    Model keys

--- HaarlemFestival/Model/TimeSlot.cs
+++ HaarlemFestival/Model/TimeSlot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,18 @@ namespace HaarlemFestival.Model
 {
     public class TimeSlot
     {
+        [Key]
         public int ActivityId { get; set; }
+        public virtual Activity Activity { get; set; }
+        [Key]
         public DateTime StartTime { get; set; }
+        [Required]
         public DateTime EndTime { get; set; }
+        [Required]
         public int TotalSeats { get; set; }
+        [Required]
         public int FreeSeats { get; set; }
+
         public string Hall { get; set; }   // somige locaties hebben verschillende hallen binnen die locatie
 
     }
