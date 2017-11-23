83a94b81b8d7a4abddb6287bf24dcd69629b2e04
Joost96 <joost-k@live.nl>
2017-11-23 14:11:06 +0100

    foreign key fixes

--- HaarlemFestival/Model/TimeSlot.cs
+++ HaarlemFestival/Model/TimeSlot.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HaarlemFestival.Model
 {
     public class TimeSlot
     {
-        [Key]
-        public int ActivityId { get; set; }
+        [Column(Order =0), Key, ForeignKey("Activity")]
+        public int Activity_Id { get; set; }
         public virtual Activity Activity { get; set; }
-        [Key]
+        [Column(Order = 1), Key]
         public DateTime StartTime { get; set; }
         [Required]
         public DateTime EndTime { get; set; }
