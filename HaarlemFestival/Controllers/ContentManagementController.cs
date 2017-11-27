4030fc013f54aff3ef894204ec50192b25df8686
Joost96 <joost-k@live.nl>
2017-11-27 16:56:10 +0100

    small start content mananger

--- HaarlemFestival/Controllers/ContentManagementController.cs
+++ HaarlemFestival/Controllers/ContentManagementController.cs
@@ -1,4 +1,5 @@
 ﻿using HaarlemFestival.Model;
+using HaarlemFestival.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,19 @@ namespace HaarlemFestival.Controllers
     {
 
         private DBHF db;
+        private IPageRepository pageRepository;
+
+        public ContentManagementController()
+        {
+            db = new DBHF();
+            pageRepository = new PageRepository(db);
+        }
+
         // GET: ContentManager
         public ActionResult Index()
         {
-
-            return View();
+            Page homePage = pageRepository.GetPage("Home");
+            return View(homePage);
         }
     }
 }
