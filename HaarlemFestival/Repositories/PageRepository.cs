abd646c2674f8b26340aa7e20b0995e3048ac61f
Joost96 <joost-k@live.nl>
2017-11-27 16:17:45 +0100

    db mix (hoop ik)

---
+++ HaarlemFestival/Repositories/PageRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using HaarlemFestival.Model;
+
+namespace HaarlemFestival.Repositories
+{
+    public class PageRepository : IPageRepository
+    {
+        private DBHF db;
+        public PageRepository(DBHF db)
+        {
+            this.db = db;
+        }
+        public Page GetPage(string name)
+        {
+            return db.Pages.Include(p => p.PageDescriptions).FirstOrDefault(p => p.Name.Equals(name));
+        }
+
+        public void UpdatePage(Page page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
