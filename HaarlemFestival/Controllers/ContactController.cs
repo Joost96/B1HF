448b0900f43412875db9a02d83421e2480e88fb3
oleksie <alexabdalla95@gmail.com>
2017-11-29 16:06:24 +0100

    COntact page layout af

--- HaarlemFestival/Controllers/ContactController.cs
+++ HaarlemFestival/Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HaarlemFestival.Model;
+using HaarlemFestival.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,17 @@ using System.Web.Mvc;
 
 namespace HaarlemFestival.Controllers
 {
+    
     public class ContactController : Controller
     {
         // GET: Contact
         public ActionResult Contact()
         {
-            return View();
+            DBHF db = new DBHF();
+            IPageRepository pageRepo = new PageRepository(db);
+            Page page = pageRepo.GetPage("Contact", Language.Eng);
+
+            return View(page);
         }
     }
 }
