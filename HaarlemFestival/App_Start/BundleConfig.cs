1d62985d1000c327c15bd2260d6a6a191f7f2bb2
Joost96 <joost-k@live.nl>
2017-12-01 16:55:45 +0100

    beheer html/css + image upload

--- HaarlemFestival/App_Start/BundleConfig.cs
+++ HaarlemFestival/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace HaarlemFestival
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
