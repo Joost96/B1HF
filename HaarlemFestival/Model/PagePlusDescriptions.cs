cb0caf5a7189ed1f81e1901701379804419d2caa
Florian van Randen <fvanranden@gmail.com>
2017-12-02 18:04:11 +0100

    hele pagina af

---
+++ HaarlemFestival/Model/PagePlusDescriptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Model
+{
+    public class PagePlusDescriptions
+    {
+        public Page Page { get; set; }
+        public List<Activity> ActivityDescriptions { get; set; }
+       
+    }
+}
