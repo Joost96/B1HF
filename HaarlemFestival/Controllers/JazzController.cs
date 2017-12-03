56568952a2f0f76a05ac7d1a1b993335bdb88f87
oleksie <alexabdalla95@gmail.com>
2017-12-03 17:54:57 +0100

    Jazz pagina gevuld met info db

--- HaarlemFestival/Controllers/JazzController.cs
+++ HaarlemFestival/Controllers/JazzController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HaarlemFestival.Model;
+using HaarlemFestival.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,23 @@ namespace HaarlemFestival.Controllers
     public class JazzController : Controller
     {
         // GET: Jazz
-        public ActionResult Jazz()
+        public ActionResult Index()
         {
-            return View();
+            PagePlusDescriptions PageDescriptions = new PagePlusDescriptions();
+
+            DBHF db = new DBHF();
+            IPageRepository pageRepo = new PageRepository(db);
+            Page page = pageRepo.GetPage("Jazz", Language.Eng);
+
+            IActivityRepository activityRepo = new ActivityRepository(db);
+            IEnumerable<Activity> activities = activityRepo.GetActivities(EventType.Jazz);
+
+            activities.OrderBy(Activitie => Activitie.Timeslots);
+
+            PageDescriptions.Page = page;
+            PageDescriptions.ActivityDescriptions = activities.ToList();
+
+            return View(PageDescriptions);
         }
     }
 }
