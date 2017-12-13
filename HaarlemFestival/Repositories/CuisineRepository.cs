14d04b21ecb90402c6053d82a3392bef72dd4641
juanalb <Juan@DESKTOP-LP3D7E0>
2017-12-13 13:56:56 +0100

    Database migrations

---
+++ HaarlemFestival/Repositories/CuisineRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using HaarlemFestival.Model;
+
+namespace HaarlemFestival.Repositories
+{
+    public class CuisineRepository : ICuisineRepository
+    {
+        private DBHF db;
+        public CuisineRepository(DBHF db)
+        {
+            this.db = db;
+        }
+        public List<Cuisine> GetCuisines()
+        {
+            return db.Cuisines.ToList();
+                
+                //Where(c => c.Id.Equals(id))
+                //.Select(c => new {
+                //    c = c..Where
+                //    //d = p.PageDescriptions.Where(pd => pd.Language == language)
+                //    //    .OrderBy(pd => pd.Section)
+                //} ).AsEnumerable()
+                //.Select(x => x.p).First();
+        }
+    }
+}
