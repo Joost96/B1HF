9d35c5d86effec1fbabe91e61cf8f511e6e5bf20
juanalb <Juan@DESKTOP-LP3D7E0>
2017-12-13 13:54:57 +0100

    database migrations

--- HaarlemFestival/Repositories/ActivityRepository.cs
+++ HaarlemFestival/Repositories/ActivityRepository.cs
@@ -22,16 +22,25 @@ namespace HaarlemFestival.Repositories
         {
             return db.Activities
                 .Include(a => a.Location)
+                .Include(a => a.Cuisines)
                 .Where(a => a.Type == type)
                 .Select(a => new {
                     a,
+                    //c = .Cuisines,
                     d = a.ActivityDescriptions.Where(ad => ad.Language == language)
                                 .OrderBy(ad => ad.Section),
                     ts = a.Timeslots,
                     ti = a.Timeslots.Select(ts => ts.Tickets)
+                    
                 }).AsEnumerable()
                 .Select(x => x.a);
         }
+        //Company data = dbContext.company.Include(x => x.companyPkdClassification.Select(y => y.company)).Include(x => x.companyPkdClassification.Select(y => y.pkdClassification)).Where(i => i.id == id).FirstOrDefault();
+
+        //public IEnumerable<Activity> GetActivities(EventType type, Language language)
+        //{
+        //    return db.Accounts
+        //}
 
         public IEnumerable<Activity> GetActivities(EventType type, Language language, DateTime dag)
         {
