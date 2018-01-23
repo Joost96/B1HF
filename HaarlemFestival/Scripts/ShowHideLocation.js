bf50340fd11433606210b286d4778dd0581ae363
Davut Demir <580115@student.inholland.nl>
2018-01-23 13:34:09 +0100

    bezig met modal historic

--- HaarlemFestival/Scripts/ShowHideLocation.js
+++ HaarlemFestival/Scripts/ShowHideLocation.js
@@ -1,9 +1,11 @@
-﻿function HideAll() {
+﻿// verbergt alle informatie van de locaties
+function HideAll() {
     $("*[class^='HistoricRouteInfo']").each(function () {
         $(this).hide();
     });
 }
 
+// laat de informatie zien van de locatie waar de gebruiker met zijn muis over een drop marker heen gaat
 $(".showhide").mouseover(function () {
     var id = $(this).attr("id").substring(3);
     var infor = $(".HistoricRouteInfo" + id);
