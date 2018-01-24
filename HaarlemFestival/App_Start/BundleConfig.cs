ac45095a6971c660f22f51896ca5b3074bf3c4d5
Joost96 <joost-k@live.nl>
2018-01-24 11:00:26 +0100

    datetimepicker and little talking management

--- HaarlemFestival/App_Start/BundleConfig.cs
+++ HaarlemFestival/App_Start/BundleConfig.cs
@@ -10,6 +10,10 @@ namespace HaarlemFestival
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
+                        "~/Scripts/jquery-ui-*",
+                        "~/Scripts/jquery.datetimepicker.full.js",
+                        "~/Scripts/jquery.datetimepicker.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +30,7 @@ namespace HaarlemFestival
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
+                      "~/Content/jquery.datetimepicker.min.css",
                       "~/Content/site.css"));
         }
     }
